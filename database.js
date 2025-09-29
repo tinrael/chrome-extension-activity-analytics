55492e87e63b50f5e59642b2a6a94bfb336f728a
Tymur Krasnopir <krasnopir.std@gmail.com>
2025-09-29 10:32:29 +0300

    Update the openIndexedDB function

--- database.js
+++ database.js
@@ -4,7 +4,18 @@ const OBJECT_STORE_NAME = "events";
 function openIndexedDB() {
   return new Promise((resolve, reject) => {
     const IDBOpenDBRequest = indexedDB.open(DATABASE_NAME);
-
+    IDBOpenDBRequest.onupgradeneeded = (IDBVersionChangeEvent) => {
+      if (
+        !IDBVersionChangeEvent.target.result.objectStoreNames.contains(
+          OBJECT_STORE_NAME
+        )
+      ) {
+        IDBVersionChangeEvent.target.result.createObjectStore(
+          OBJECT_STORE_NAME,
+          { keyPath: "id", autoIncrement: true }
+        );
+      }
+    };
     IDBOpenDBRequest.onsuccess = (event) => resolve(event.target.result);
     IDBOpenDBRequest.onerror = (event) => reject(event.target.error);
   });
