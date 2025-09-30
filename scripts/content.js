60aa0459bdd87801ea291e1363f46d16457de165
Tymur Krasnopir <krasnopir.std@gmail.com>
2025-09-30 14:39:23 +0300

    Track focus_gain, focus_lost, visibility_change

--- scripts/content.js
+++ scripts/content.js
@@ -28,4 +28,35 @@ document.addEventListener("DOMContentLoaded", () => {
       page: { url: location.href, title: document.title },
     });
   });
+
+  document.addEventListener("visibilitychange", () => {
+    chrome.runtime.sendMessage({
+      v: SCHEMA_VERSION,
+      sid: sessionId,
+      ts: Date.now(),
+      type: "visibility_change",
+      visibilityState: document.visibilityState,
+      page: { url: location.href, title: document.title },
+    });
+  });
+
+  window.addEventListener("focus", () => {
+    chrome.runtime.sendMessage({
+      v: SCHEMA_VERSION,
+      sid: sessionId,
+      ts: Date.now(),
+      type: "focus_gain",
+      page: { url: location.href, title: document.title },
+    });
+  });
+
+  window.addEventListener("blur", () => {
+    chrome.runtime.sendMessage({
+      v: SCHEMA_VERSION,
+      sid: sessionId,
+      ts: Date.now(),
+      type: "focus_lost",
+      page: { url: location.href, title: document.title },
+    });
+  });
 });
