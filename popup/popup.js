e360f3dc3446b1b7f847814f56b731da20801efb
Tymur Krasnopir <krasnopir.std@gmail.com>
2025-10-01 12:27:08 +0300

    Add a button to download events as a JSON file

---
+++ popup/popup.js
@@ -0,0 +1,18 @@
+import { getAllEvents } from "../database.js";
+
+document.getElementById("download-json").addEventListener("click", async () => {
+  try {
+    const events = await getAllEvents();
+    const blob = new Blob([JSON.stringify(events, null, 2)], {
+      type: "application/json",
+    });
+    const objectURL = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = objectURL;
+    a.download = `events.json`;
+    a.click();
+    URL.revokeObjectURL(objectURL);
+  } catch (error) {
+    console.error(error);
+  }
+});
