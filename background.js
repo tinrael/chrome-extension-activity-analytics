927cb53cc8e10b039b3c5b7afa388b0a33f55316
Tymur Krasnopir <krasnopir.std@gmail.com>
2025-10-02 17:30:55 +0300

    Track active_time_ms

--- background.js
+++ background.js
@@ -1,5 +1,20 @@
 import { addEvent } from "./database.js";
 
+chrome.idle.setDetectionInterval(15);
+
+chrome.idle.onStateChanged.addListener((newState) => {
+  chrome.tabs.query({}, (tabs) => {
+    for (const tab of tabs) {
+      if (tab.id) {
+        chrome.tabs.sendMessage(tab.id, {
+          topic: "idle-state",
+          idleState: newState,
+        });
+      }
+    }
+  });
+});
+
 chrome.runtime.onMessage.addListener((message) => {
   addEvent(message).then(() => {
     console.log(message);
--- scripts/content.js
+++ scripts/content.js
@@ -6,6 +6,13 @@ if (!sessionStorage.getItem("sessionId")) {
 
 const sessionId = sessionStorage.getItem("sessionId");
 
+let isUserActive = true;
+
+let isDocumentVisible = document.visibilityState === "visible";
+let hasDocumentFocus = document.hasFocus();
+
+let time = null;
+
 document.addEventListener("DOMContentLoaded", () => {
   chrome.runtime.sendMessage({
     v: SCHEMA_VERSION,
@@ -14,6 +21,8 @@ document.addEventListener("DOMContentLoaded", () => {
     type: "page_view",
     page: { url: location.href, title: document.title },
   });
+
+  checkTime();
 
   document.addEventListener("click", (event) => {
     chrome.runtime.sendMessage({
@@ -30,6 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   document.addEventListener("visibilitychange", () => {
+    isDocumentVisible = document.visibilityState === "visible";
     chrome.runtime.sendMessage({
       v: SCHEMA_VERSION,
       sid: sessionId,
@@ -38,9 +48,11 @@ document.addEventListener("DOMContentLoaded", () => {
       visibilityState: document.visibilityState,
       page: { url: location.href, title: document.title },
     });
+    checkTime();
   });
 
   window.addEventListener("focus", () => {
+    hasDocumentFocus = true;
     chrome.runtime.sendMessage({
       v: SCHEMA_VERSION,
       sid: sessionId,
@@ -48,9 +60,11 @@ document.addEventListener("DOMContentLoaded", () => {
       type: "focus_gain",
       page: { url: location.href, title: document.title },
     });
+    checkTime();
   });
 
   window.addEventListener("blur", () => {
+    hasDocumentFocus = false;
     chrome.runtime.sendMessage({
       v: SCHEMA_VERSION,
       sid: sessionId,
@@ -58,5 +72,33 @@ document.addEventListener("DOMContentLoaded", () => {
       type: "focus_lost",
       page: { url: location.href, title: document.title },
     });
+    checkTime();
   });
 });
+
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.topic === "idle-state") {
+    isUserActive = message.idleState === "active";
+    checkTime();
+  }
+});
+
+function checkTime() {
+  const now = Date.now();
+  if (isUserActive && isDocumentVisible && hasDocumentFocus && !time) {
+    time = now;
+  } else if (
+    (!isUserActive || !isDocumentVisible || !hasDocumentFocus) &&
+    time
+  ) {
+    chrome.runtime.sendMessage({
+      v: SCHEMA_VERSION,
+      sid: sessionId,
+      ts: now,
+      type: "active_time_ms",
+      duration: now - time,
+      page: { url: location.href, title: document.title },
+    });
+    time = null;
+  }
+}
